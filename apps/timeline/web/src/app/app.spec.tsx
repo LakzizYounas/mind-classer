3733f251e1057ca0c345a59af39b47fc68b34f5c
Younas <younaslakziz@gmail.com>
2022-01-23 22:32:12 +0100

    [ADD] Nx & timeline-web app

--- apps/timeline/web/src/app/app.spec.tsx
+++ apps/timeline/web/src/app/app.spec.tsx
@@ -12,6 +12,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome timeline/gi)).toBeTruthy();
+    expect(getByText(/Welcome timeline-web/gi)).toBeTruthy();
   });
 });
---
+++ apps/timeline/web/src/app/app.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return <div>Hi</div>;
+}
+
+export default App;
