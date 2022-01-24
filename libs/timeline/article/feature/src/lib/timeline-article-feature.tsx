29217353d0ba82f56f152f7dd80bf11b91322fd6
Younas <younaslakziz@gmail.com>
2022-01-24 23:21:12 +0100

    Add timelinearticle page

--- libs/timeline/article/feature/src/lib/timeline-article-feature.tsx
+++ libs/timeline/article/feature/src/lib/timeline-article-feature.tsx
@@ -1,9 +1,30 @@
+import { List } from 'immutable';
 import React from 'react';
 
+export interface IBlock {
+  id: string;
+  html: string;
+  tag: string;
+}
+
+const initialBlock: IBlock = {
+  id: 'random-id',
+  html: '',
+  tag: 'p',
+};
+
 export const TimelineArticleFeature: React.FC = () => {
+  const [blocks, setBlocks] = React.useState(List<IBlock>([initialBlock]));
+
   return (
     <div>
-      <h1>Welcome to TimelineArticleFeature!</h1>
+      {blocks.map((block) => {
+        return (
+          <div key={block.id} id={block.id}>
+            Tag: {block.tag}, Content: {block.html}
+          </div>
+        );
+      })}
     </div>
   );
 };
