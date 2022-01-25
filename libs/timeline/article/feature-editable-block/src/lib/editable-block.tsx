d8dd3366794048bdcd6f075e3d0af64f214295c4
Younas <younaslakziz@gmail.com>
2022-01-25 23:33:47 +0100

    big chantier going on

--- libs/timeline/article/feature-editable-block/src/lib/editable-block.tsx
+++ libs/timeline/article/feature-editable-block/src/lib/editable-block.tsx
@@ -1,27 +1,65 @@
-import React, { useRef, useState } from 'react';
+import React, { RefObject, useRef, useState } from 'react';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 
 export interface IBlock {
   id: string;
   html: string;
   tag: string;
+  ref?: HTMLElement | null;
 }
 
-export const EditableBlock: React.FC<IBlock> = (initialBlock) => {
-  const content = useRef(null);
-  const [html, setHtml] = useState(initialBlock.html);
-  const [tag, setTag] = useState(initialBlock.tag);
+export interface IBlockActions {
+  updagePage: (block: IBlock) => void;
+  addBlock: (block: Partial<IBlock>) => void;
+  deleteBlock: (block: Partial<IBlock>) => void;
+}
+
+export const EditableBlock: React.FC<IBlock & IBlockActions> = (props) => {
+  const content = useRef<HTMLElement>(null);
+  const [html, setHtml] = useState(props.html);
+  const [htmlBackup, setHtmlBackup] = useState<string>();
+  const [previousKey, setPreviousKey] = useState('');
+  const [tag, setTag] = useState(props.tag);
 
   const handleChange = (event: ContentEditableEvent) => {
     setHtml(event.target.value);
   };
 
+  function onKeyDownHandler(e: any) {
+    console.log('🚀 ~ file: editable-block.tsx ~ line 20 ~ html', html);
+    if (e.key === '/') {
+      setHtmlBackup(html);
+    }
+    if (e.key === 'Enter') {
+      if (previousKey !== 'Shift') {
+        e.preventDefault();
+        props.addBlock({
+          id: props.id,
+          ref: content.current,
+        });
+      }
+    }
+    if (e.key === 'Backspace' && html === '') {
+      console.log('INSIDE ????');
+      e.preventDefault();
+      props.deleteBlock({
+        id: props.id,
+        ref: content.current,
+      });
+    }
+    setPreviousKey(e.key);
+  }
+
   return (
-    <ContentEditable
-      innerRef={content}
-      html={html}
-      tagName={tag}
-      onChange={handleChange}
-    />
+    <div style={{ backgroundColor: 'grey' }}>
+      <ContentEditable
+        className="Block"
+        innerRef={content}
+        html={html}
+        tagName={tag}
+        onChange={handleChange}
+        onKeyDown={onKeyDownHandler}
+      />
+    </div>
   );
 };
--- libs/timeline/article/feature-editable-page/src/lib/editable-page.tsx
+++ libs/timeline/article/feature-editable-page/src/lib/editable-page.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { List } from 'immutable';
 
-import { IBlock } from '@mind-class/timeline/article/feature-editable-block';
+import {
+  EditableBlock,
+  IBlock,
+} from '@mind-class/timeline/article/feature-editable-block';
 
 const initialBlock: IBlock = {
   id: 'random-id',
@@ -9,16 +12,67 @@ const initialBlock: IBlock = {
   tag: 'p',
 };
 
+const setCaretToEnd = (element: any) => {
+  const range = document.createRange();
+  const selection = window.getSelection() as any;
+
+  range.selectNodeContents(element);
+  range.collapse(false);
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  element.focus();
+};
+
 export const EditablePage: React.FC = () => {
-  const [blocks, setBlocks] = React.useState(List<IBlock>([initialBlock]));
+  const [blocks, setBlocks] = useState(List<IBlock>([initialBlock]));
+
+  const updatePageHandler = (updatedBlock: IBlock) => {
+    const index = blocks.map((b) => b.id).indexOf(updatedBlock.id);
+
+    setBlocks(
+      blocks.update(index, (block) => ({
+        ...(block as IBlock),
+        tag: updatedBlock.tag,
+        html: updatedBlock.html,
+      }))
+    );
+  };
+
+  const addBlockHandler = (currentBlock: any) => {
+    const newBlock = { id: 'randomid pas si random', html: '', tag: 'p' };
+    const index = blocks.map((b) => b.id).indexOf(currentBlock.id);
+
+    setBlocks(blocks.splice(index + 1, 0, newBlock));
+    // currentBlock.ref.nextElementSibling?.focus();
+  };
+
+  const deleteBlockHandler = (currentBlock: any) => {
+    const previousBlock = currentBlock.ref.previousElementSibling;
+    if (previousBlock) {
+      const index = blocks.map((b) => b.id).indexOf(currentBlock.id);
+
+      setBlocks(blocks.splice(index, 1));
+
+      setCaretToEnd(previousBlock);
+      previousBlock.focus();
+    }
+  };
 
   return (
     <div>
       {blocks.map((block) => {
         return (
-          <div key={block.id} id={block.id}>
-            Tag: {block.tag}, Content: {block.html}
-          </div>
+          <EditableBlock
+            key={block.id}
+            id={block.id}
+            tag={block.tag}
+            html={block.html}
+            updagePage={updatePageHandler}
+            addBlock={addBlockHandler}
+            deleteBlock={deleteBlockHandler}
+          />
         );
       })}
     </div>
