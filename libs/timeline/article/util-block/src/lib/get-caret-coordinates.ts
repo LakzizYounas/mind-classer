2f332b3ec1bad33bca05246ae6bddccb6a14c8a9
Younas <younaslakziz@gmail.com>
2022-01-27 23:02:28 +0100

    Add util block library

---
+++ libs/timeline/article/util-block/src/lib/get-caret-coordinates.ts
@@ -0,0 +1,17 @@
+export const getCaretCoordinates = () => {
+  let x = 0;
+  let y = 0;
+  const selection = window.getSelection();
+
+  if (selection?.rangeCount !== 0) {
+    const range = selection?.getRangeAt(0).cloneRange();
+    range?.collapse(false);
+    const rect = range?.getClientRects()[0];
+
+    if (rect) {
+      x = rect.left;
+      y = rect.top;
+    }
+  }
+  return { x, y };
+};
---
+++ libs/timeline/article/util-block/src/lib/index.ts
@@ -0,0 +1,2 @@
+export * from './set-caret-to-end';
+export * from './get-caret-coordinates';
